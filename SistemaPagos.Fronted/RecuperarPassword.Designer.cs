c093a5d3dada99a893d0ae81ab92b88aec598c7e
Karen Ortega <Karen Ortega@LAPTOP-LSMCH8TR>
2026-05-26 12:07:28 -0600

    Colores terminados en RecuperarPassaword

--- SistemaPagos.Fronted/RecuperarPassword.Designer.cs
+++ SistemaPagos.Fronted/RecuperarPassword.Designer.cs
@@ -34,21 +34,23 @@
             btnEnviar = new Button();
             lblMensaje = new Label();
             groupBox1 = new GroupBox();
+            txtConfirmarContrasena = new TextBox();
             btnGuardar = new Button();
             label4 = new Label();
             txtNuevaContrasena = new TextBox();
-            label3 = new Label();
             btnCancelar = new Button();
-            txtConfirmarContrasena = new TextBox();
             groupBox1.SuspendLayout();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(336, 21);
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Century Schoolbook", 13.8F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.FromArgb(192, 0, 192);
+            label1.Location = new Point(236, 9);
             label1.Name = "label1";
-            label1.Size = new Size(188, 20);
+            label1.Size = new Size(355, 28);
             label1.TabIndex = 0;
             label1.Text = "RECUPERAR CONTRASEÑA";
             label1.Click += label1_Click;
@@ -56,16 +58,18 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(49, 56);
+            label2.Font = new Font("Century Schoolbook", 10.2F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.FromArgb(192, 0, 192);
+            label2.Location = new Point(52, 56);
             label2.Name = "label2";
-            label2.Size = new Size(285, 20);
+            label2.Size = new Size(385, 20);
             label2.TabIndex = 1;
             label2.Text = "Ingrese su número de identificación (CUI):";
             label2.Click += label2_Click;
             // 
             // txtIdentificacion
             // 
-            txtIdentificacion.Location = new Point(53, 82);
+            txtIdentificacion.Location = new Point(55, 79);
             txtIdentificacion.Name = "txtIdentificacion";
             txtIdentificacion.PlaceholderText = "CUI";
             txtIdentificacion.Size = new Size(173, 27);
@@ -74,19 +78,23 @@
             // 
             // btnEnviar
             // 
-            btnEnviar.Location = new Point(343, 75);
+            btnEnviar.BackColor = Color.Fuchsia;
+            btnEnviar.Font = new Font("Century Schoolbook", 10.2F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            btnEnviar.ForeColor = Color.White;
+            btnEnviar.Location = new Point(451, 97);
             btnEnviar.Name = "btnEnviar";
             btnEnviar.Size = new Size(94, 29);
             btnEnviar.TabIndex = 3;
             btnEnviar.Text = "Enviar ";
-            btnEnviar.UseVisualStyleBackColor = true;
+            btnEnviar.UseVisualStyleBackColor = false;
             // 
             // lblMensaje
             // 
             lblMensaje.AutoSize = true;
+            lblMensaje.Font = new Font("Century Schoolbook", 10.2F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
             lblMensaje.Location = new Point(52, 118);
             lblMensaje.Name = "lblMensaje";
-            lblMensaje.Size = new Size(21, 20);
+            lblMensaje.Size = new Size(23, 20);
             lblMensaje.TabIndex = 4;
             lblMensaje.Text = "\"\"";
             lblMensaje.Click += lblMensaje_Click_1;
@@ -97,7 +105,8 @@
             groupBox1.Controls.Add(btnGuardar);
             groupBox1.Controls.Add(label4);
             groupBox1.Controls.Add(txtNuevaContrasena);
-            groupBox1.Controls.Add(label3);
+            groupBox1.Font = new Font("Century Schoolbook", 10.2F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            groupBox1.ForeColor = Color.FromArgb(192, 0, 192);
             groupBox1.Location = new Point(55, 175);
             groupBox1.Name = "groupBox1";
             groupBox1.Size = new Size(665, 205);
@@ -107,69 +116,67 @@
             groupBox1.Visible = false;
             groupBox1.Enter += groupBox1_Enter;
             // 
+            // txtConfirmarContrasena
+            // 
+            txtConfirmarContrasena.Location = new Point(0, 109);
+            txtConfirmarContrasena.Name = "txtConfirmarContrasena";
+            txtConfirmarContrasena.PlaceholderText = "Confirma tu Contraseña";
+            txtConfirmarContrasena.Size = new Size(167, 28);
+            txtConfirmarContrasena.TabIndex = 4;
+            txtConfirmarContrasena.UseSystemPasswordChar = true;
+            txtConfirmarContrasena.TextChanged += txtConfirmarContrasena_TextChanged_1;
+            // 
             // btnGuardar
             // 
-            btnGuardar.Location = new Point(8, 171);
+            btnGuardar.BackColor = Color.Fuchsia;
+            btnGuardar.Font = new Font("Century Schoolbook", 10.2F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            btnGuardar.ForeColor = Color.White;
+            btnGuardar.Location = new Point(-2, 157);
             btnGuardar.Name = "btnGuardar";
-            btnGuardar.Size = new Size(94, 29);
+            btnGuardar.Size = new Size(100, 29);
             btnGuardar.TabIndex = 3;
             btnGuardar.Text = "Guardar\r\n";
-            btnGuardar.UseVisualStyleBackColor = true;
+            btnGuardar.UseVisualStyleBackColor = false;
             btnGuardar.Click += btnGuardar_Click;
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(12, 96);
+            label4.Location = new Point(0, 75);
             label4.Name = "label4";
-            label4.Size = new Size(153, 20);
+            label4.Size = new Size(210, 20);
             label4.TabIndex = 2;
             label4.Text = "Confirmar Contraseña";
             // 
             // txtNuevaContrasena
             // 
-            txtNuevaContrasena.Location = new Point(14, 61);
+            txtNuevaContrasena.Location = new Point(0, 26);
             txtNuevaContrasena.Name = "txtNuevaContrasena";
             txtNuevaContrasena.PlaceholderText = "Escribe tu nueva contraseña";
-            txtNuevaContrasena.Size = new Size(204, 27);
+            txtNuevaContrasena.Size = new Size(204, 28);
             txtNuevaContrasena.TabIndex = 1;
             txtNuevaContrasena.UseSystemPasswordChar = true;
             txtNuevaContrasena.TextChanged += textBox1_TextChanged_1;
             // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Location = new Point(13, 31);
-            label3.Name = "label3";
-            label3.Size = new Size(129, 20);
-            label3.TabIndex = 0;
-            label3.Text = "Nueva Contraseña";
-            // 
             // btnCancelar
             // 
+            btnCancelar.BackColor = Color.Fuchsia;
             btnCancelar.DialogResult = DialogResult.Cancel;
-            btnCancelar.Location = new Point(55, 389);
+            btnCancelar.Font = new Font("Century Schoolbook", 10.2F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            btnCancelar.ForeColor = Color.White;
+            btnCancelar.Location = new Point(620, 386);
             btnCancelar.Name = "btnCancelar";
-            btnCancelar.Size = new Size(94, 29);
+            btnCancelar.Size = new Size(100, 29);
             btnCancelar.TabIndex = 8;
             btnCancelar.Text = "Cancelar";
-            btnCancelar.UseVisualStyleBackColor = true;
+            btnCancelar.UseVisualStyleBackColor = false;
             btnCancelar.Click += button2_Click;
-            // 
-            // txtConfirmarContrasena
-            // 
-            txtConfirmarContrasena.Location = new Point(12, 130);
-            txtConfirmarContrasena.Name = "txtConfirmarContrasena";
-            txtConfirmarContrasena.PlaceholderText = "Confirma tu Contraseña";
-            txtConfirmarContrasena.Size = new Size(167, 27);
-            txtConfirmarContrasena.TabIndex = 4;
-            txtConfirmarContrasena.UseSystemPasswordChar = true;
-            txtConfirmarContrasena.TextChanged += txtConfirmarContrasena_TextChanged_1;
             // 
             // RecuperarPassword
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Linen;
             ClientSize = new Size(800, 450);
             Controls.Add(btnCancelar);
             Controls.Add(groupBox1);
@@ -195,7 +202,6 @@
         private Button btnEnviar;
         private Label lblMensaje;
         private GroupBox groupBox1;
-        private Label label3;
         private TextBox txtNuevaContrasena;
         private Label label4;
         private Button btnGuardar;
