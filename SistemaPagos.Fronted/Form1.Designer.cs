042af82fe0d94fd6de24f9a5ce60c17a59870524
Karen Ortega <Karen Ortega@LAPTOP-LSMCH8TR>
2026-05-28 10:48:39 -0600

    Color terminado en Forms1.cs

--- SistemaPagos.Fronted/Form1.Designer.cs
+++ SistemaPagos.Fronted/Form1.Designer.cs
@@ -43,11 +43,12 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.BackColor = Color.FromArgb(192, 192, 255);
-            label1.Font = new Font("Bahnschrift Condensed", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label1.Location = new Point(487, 20);
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Chiller", 36F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.FromArgb(192, 0, 192);
+            label1.Location = new Point(446, 9);
             label1.Name = "label1";
-            label1.Size = new Size(171, 41);
+            label1.Size = new Size(281, 69);
             label1.TabIndex = 0;
             label1.Text = "Iniciar Sesión";
             label1.Click += label1_Click_1;
@@ -55,56 +56,67 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(487, 83);
+            label2.Font = new Font("Century Schoolbook", 10.2F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.Fuchsia;
+            label2.Location = new Point(469, 93);
             label2.Name = "label2";
-            label2.Size = new Size(184, 20);
+            label2.Size = new Size(247, 20);
             label2.TabIndex = 1;
             label2.Text = "No. de Identificación (CUI)";
             // 
             // txtIdentificacion
             // 
-            txtIdentificacion.Location = new Point(467, 106);
+            txtIdentificacion.Font = new Font("Century Schoolbook", 10.2F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            txtIdentificacion.Location = new Point(488, 125);
             txtIdentificacion.Name = "txtIdentificacion";
-            txtIdentificacion.Size = new Size(204, 27);
+            txtIdentificacion.Size = new Size(204, 28);
             txtIdentificacion.TabIndex = 2;
             txtIdentificacion.TextChanged += txtIdentificacion_TextChanged;
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(530, 161);
+            label3.Font = new Font("Century Schoolbook", 10.2F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.Fuchsia;
+            label3.Location = new Point(528, 165);
             label3.Name = "label3";
-            label3.Size = new Size(83, 20);
+            label3.Size = new Size(111, 20);
             label3.TabIndex = 3;
             label3.Text = "Contraseña";
             label3.Click += label3_Click;
             // 
             // txtContrasena
             // 
-            txtContrasena.Location = new Point(487, 184);
+            txtContrasena.Font = new Font("Century Schoolbook", 10.2F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            txtContrasena.Location = new Point(497, 190);
             txtContrasena.Name = "txtContrasena";
-            txtContrasena.Size = new Size(171, 27);
+            txtContrasena.Size = new Size(171, 28);
             txtContrasena.TabIndex = 4;
             txtContrasena.UseSystemPasswordChar = true;
             txtContrasena.TextChanged += txtContrasena_TextChanged;
             // 
             // btnIngresar
             // 
-            btnIngresar.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            btnIngresar.Location = new Point(519, 226);
+            btnIngresar.BackColor = Color.Fuchsia;
+            btnIngresar.Font = new Font("Century Schoolbook", 10.2F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            btnIngresar.ForeColor = Color.White;
+            btnIngresar.Location = new Point(528, 233);
             btnIngresar.Name = "btnIngresar";
-            btnIngresar.Size = new Size(94, 29);
+            btnIngresar.Size = new Size(127, 29);
             btnIngresar.TabIndex = 5;
             btnIngresar.Text = "INGRESAR";
-            btnIngresar.UseVisualStyleBackColor = true;
+            btnIngresar.UseVisualStyleBackColor = false;
             btnIngresar.Click += btnIngresar_Click;
             // 
             // llOlvideContrasena
             // 
             llOlvideContrasena.AutoSize = true;
-            llOlvideContrasena.Location = new Point(495, 268);
+            llOlvideContrasena.DisabledLinkColor = Color.Blue;
+            llOlvideContrasena.Font = new Font("Century Schoolbook", 10.2F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            llOlvideContrasena.LinkColor = Color.FromArgb(192, 0, 192);
+            llOlvideContrasena.Location = new Point(497, 283);
             llOlvideContrasena.Name = "llOlvideContrasena";
-            llOlvideContrasena.Size = new Size(151, 20);
+            llOlvideContrasena.Size = new Size(201, 20);
             llOlvideContrasena.TabIndex = 6;
             llOlvideContrasena.TabStop = true;
             llOlvideContrasena.Text = "Olvidé mi Contraseña";
@@ -113,9 +125,11 @@
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(495, 311);
+            label4.Font = new Font("Century Schoolbook", 10.2F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label4.ForeColor = Color.Black;
+            label4.Location = new Point(497, 321);
             label4.Name = "label4";
-            label4.Size = new Size(138, 20);
+            label4.Size = new Size(180, 20);
             label4.TabIndex = 7;
             label4.Text = "¿No tienes usuario?";
             label4.Click += label4_Click;
@@ -123,9 +137,12 @@
             // llCrearUsuario
             // 
             llCrearUsuario.AutoSize = true;
-            llCrearUsuario.Location = new Point(504, 331);
+            llCrearUsuario.DisabledLinkColor = Color.Blue;
+            llCrearUsuario.Font = new Font("Century Schoolbook", 10.2F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            llCrearUsuario.LinkColor = Color.FromArgb(192, 0, 192);
+            llCrearUsuario.Location = new Point(504, 353);
             llCrearUsuario.Name = "llCrearUsuario";
-            llCrearUsuario.Size = new Size(119, 20);
+            llCrearUsuario.Size = new Size(164, 20);
             llCrearUsuario.TabIndex = 8;
             llCrearUsuario.TabStop = true;
             llCrearUsuario.Text = "Crear mi Usuario";
@@ -134,7 +151,8 @@
             // btnVerContrasena
             // 
             btnVerContrasena.FlatStyle = FlatStyle.Flat;
-            btnVerContrasena.Location = new Point(663, 183);
+            btnVerContrasena.ForeColor = Color.Black;
+            btnVerContrasena.Location = new Point(685, 187);
             btnVerContrasena.Name = "btnVerContrasena";
             btnVerContrasena.Size = new Size(42, 31);
             btnVerContrasena.TabIndex = 9;
@@ -146,7 +164,7 @@
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = Color.FromArgb(192, 192, 255);
+            BackColor = Color.Linen;
             ClientSize = new Size(1184, 459);
             Controls.Add(btnVerContrasena);
             Controls.Add(llCrearUsuario);
